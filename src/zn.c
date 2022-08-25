0e0be008d41883e37a0f80edb8fff271fa71f95e
Will Thomas <will@willth.dev>
2022-08-25 14:17:53 -0400

    added file-writing functions

--- src/zn.c
+++ src/zn.c
@@ -25,6 +25,10 @@ typedef struct zn_sym_s {
 	uint8_t typ;
 } zn_sym_t;
 
+void (*zn_rel) (uint8_t*, zn_sym_t*, uint64_t, zn_sym_t*, uint64_t, int8_t*);
+
+void (*zn_writ) (uint8_t*, uint64_t, zn_sym_t*, uint64_t, zn_sym_t*, uint64_t, int8_t*);
+
 void zn_read_zn(uint8_t* bin, uint64_t* bn, zn_sym_t* sym, uint64_t* symn, zn_sym_t* rel, uint64_t* reln, int8_t* path, int8_t* e) {
 	FILE* f = fopen(path, "r");
 	if (!f) {
@@ -57,9 +61,55 @@ void zn_read_zn(uint8_t* bin, uint64_t* bn, zn_sym_t* sym, uint64_t* symn, zn_sy
 	memcpy(sym, fx + symoff, symnum * 17);
 	memcpy(rel, fx + reloff, relnum * 17);
 	
+	for (uint64_t i = 0; i < symnum; i++) {
+		sym[i + *symn].addr += *bn;
+		for (uint64_t j = 0; j < *symn; j++) {
+			if (sym[i + *symn].str == sym[j].str) {
+				printf("[%s] error: symbol '%s' already defined\n", path, (int8_t*) &(sym[i + *symn].str));
+				*e = -1;
+			}
+		}
+	}
+	
+	for (uint64_t i = 0; i < relnum; i++) {
+		rel[i + *reln].addr += *bn;
+	}
+	
 	*bn += binnum;
 	*symn += symnum;
 	*reln += relnum;
+}
+
+void zn_writ_bin(uint8_t* bin, uint64_t bn, zn_sym_t* sym, uint64_t symn, zn_sym_t* rel, uint64_t reln, int8_t* path) {
+	FILE* f = fopen(path, "w");
+	fwrite(bin, bn, 1, f);
+	fclose(f);
+}
+
+void zn_writ_zn(uint8_t* bin, uint64_t bn, zn_sym_t* sym, uint64_t symn, zn_sym_t* rel, uint64_t reln, int8_t* path) {
+	uint8_t* buf = malloc(52 + bn + (symn * 17) + (reln * 17));
+	
+	uint64_t binoff = 52;
+	uint64_t symoff = 52 + bn;
+	uint64_t reloff = 52 + bn + (symn * 17);
+	
+	memcpy(buf, "zinc", 4);
+	memcpy(buf + 4, &binoff, 8);
+	memcpy(buf + 12, &bn, 8);
+	memcpy(buf + 20, &symoff, 8);
+	memcpy(buf + 28, &symn, 8);
+	memcpy(buf + 36, &reloff, 8);
+	memcpy(buf + 44, &reln, 8);
+	
+	memcpy(buf + binoff, bin, bn);
+	memcpy(buf + symoff, sym, symn * 17);
+	memcpy(buf + reloff, rel, reln * 17);
+	
+	FILE* f = fopen(path, "w");
+	fwrite(buf, 52 + bn + (symn * 17) + (reln * 17), 1, f);
+	fclose(f);
+	
+	free(buf);
 }
 
 int8_t main(uint32_t argc, int8_t** argv) {
@@ -76,21 +126,21 @@ int8_t main(uint32_t argc, int8_t** argv) {
 	}
 	
 	if (!strcmp(argv[argc - 1] + strlen(argv[argc - 1]) - 4, ".bin")) {
-		
+		zn_writ = zn_writ_bin;
 	}
 	else if (!strcmp(argv[argc - 1] + strlen(argv[argc - 1]) - 3, ".zn")) {
-		
+		zn_writ = zn_writ_zn;
 	}
 	else {
 		printf("error: invalid output format\n");
 		return -1;
 	}
 	
-	uint8_t* bin = malloc(1000);
+	uint8_t* bin = malloc(1000000);
 	uint64_t bn = 0;
-	zn_sym_t* sym = malloc(1000);
+	zn_sym_t* sym = malloc(1000000);
 	uint64_t symn = 0;
-	zn_sym_t* rel = malloc(1000);
+	zn_sym_t* rel = malloc(1000000);
 	uint64_t reln = 0;
 	int8_t e = 0;
 	
@@ -106,6 +156,7 @@ int8_t main(uint32_t argc, int8_t** argv) {
 		for (uint16_t i = 0; i < reln; i++) {
 			printf("[rel]\tname: %s\n\taddr: %lu\n\ttyp: %hhu\n", (int8_t*) &(rel[i].str), rel[i].addr, rel[i].typ);
 		}
+		zn_writ(bin, bn, sym, symn, rel, reln, argv[argc - 1]);
 	}
 	
 	free(bin);
